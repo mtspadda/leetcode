ead800af02549a9824c6ac3641686ab380d683d9
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-11-07 00:04:07 -0300

    - Solved 122. Max profit 2 - Top interview 150

---
+++ src/MaxProfit2.java
@@ -0,0 +1,14 @@
+public class MaxProfit2 {
+    public int maxProfit2(int[] prices) {
+        int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                maxProfit += prices[i] - prices[i - 1];
+            }
+        }
+
+        return maxProfit;
+
+    }
+}
