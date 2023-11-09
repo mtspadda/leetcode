89c369c4bb367d681d6458d3e52766c7e124d2b8
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-11-08 23:40:53 -0300

    - Solved 55. Jump game - Top interview 150

---
+++ src/JumpGame.java
@@ -0,0 +1,15 @@
+public class JumpGame {
+    public boolean canJump(int[] nums) {
+        int maxReachable = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (i > maxReachable) {
+                return false;
+            }
+            maxReachable = Math.max(maxReachable, i + nums[i]);
+            if (maxReachable >= nums.length - 1) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
