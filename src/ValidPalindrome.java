43e4393e19f97974284220d318e485755f1079b5
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-11-09 22:58:55 -0300

    - Solved 125. Valid Palindrome - Top interview 150

---
+++ src/ValidPalindrome.java
@@ -0,0 +1,21 @@
+public class ValidPalindrome {
+    public static boolean isPalindrome(String s) {
+        // Convert the string to lowercase and remove non-alphanumeric characters
+        String cleanedString = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+
+        // Check if the cleaned string is a palindrome
+        int left = 0;
+        int right = cleanedString.length() - 1;
+
+        while (left < right) {
+            // --> left
+            // <-- right
+            if (cleanedString.charAt(left) != cleanedString.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
