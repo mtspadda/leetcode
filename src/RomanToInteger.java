5e880ddf172280c172da9c87773cbe9f4fa2d4a8
Matheus Marques <matheuspadilha.dev@gmail.com>
2023-11-13 23:36:19 -0300

    - Solved 13. Roman to Integer - Top interview 150

---
+++ src/RomanToInteger.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class RomanToInteger {
+    public static int romanToInt(String s) {
+        // Create a HashMap to store the values of each Roman numeral
+        HashMap<Character, Integer> romanMap = new HashMap<>();
+        romanMap.put('I', 1);
+        romanMap.put('V', 5);
+        romanMap.put('X', 10);
+        romanMap.put('L', 50);
+        romanMap.put('C', 100);
+        romanMap.put('D', 500);
+        romanMap.put('M', 1000);
+
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            // Check if the current Roman numeral is smaller than the next one
+            if (i < s.length() - 1 && romanMap.get(s.charAt(i)) < romanMap.get(s.charAt(i + 1))) {
+                result -= romanMap.get(s.charAt(i));
+            } else {
+                result += romanMap.get(s.charAt(i));
+            }
+        }
+
+        return result;
+    }
+}
