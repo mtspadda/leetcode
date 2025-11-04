5ef74e3799419fa0a4ab94b25a81029938fcfec6
Matheus Marques <matheuspadilha.dev@gmail.com>
2025-11-04 21:02:10 +0000

    - Solutions of TwoSum2 and LengthOfLastWord

---
+++ src/LengthOfLastWord.java
@@ -0,0 +1,21 @@
+public class LengthOfLastWord {
+
+    public int lengthofLastWord(String s){
+        int end = s.length() - 1;
+
+
+        while (end >= 0 && s.charAt(end) == ' ') {
+            end--;
+        }
+
+        int start = end;
+
+        while (start >= 0 && s.charAt(start) != ' '){
+            start--;
+        }
+
+        return end - start;
+
+    }
+
+}
