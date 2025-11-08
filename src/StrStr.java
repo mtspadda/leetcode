3ad4730efb6a379e48fcb2b97b8c8c249fc2a7ad
Matheus Marques <matheuspadilha.dev@gmail.com>
2025-11-08 23:58:47 +0000

    - Find the Index of the First Occurrence in a String Solution

---
+++ src/StrStr.java
@@ -0,0 +1,16 @@
+public class StrStr {
+    public int strStr(String haystack, String needle) {
+        int window = needle.length();
+
+        while (haystack.length() < needle.length()) {
+            return -1;
+        }
+
+        for (int i = 0; i <= haystack.length() - needle.length(); i++){
+            if (haystack.substring(i, i + window).equals(needle)){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
